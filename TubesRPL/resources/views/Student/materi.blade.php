b1f4cf07bc19269148883aeed276293772012c3b
wasikamin <wasikamin@student.telkomuniversity.ac.id>
2022-05-21 16:11:43 +0700

    update: Forum UI ref #11

--- TubesRPL/resources/views/Student/materi.blade.php
+++ TubesRPL/resources/views/Student/materi.blade.php
@@ -14,17 +14,60 @@
 
                         <div class="d-flex justify-content-between">
                             <span style="color:#9C9AAA;">{{ $video->created_at->format('l, d F Y') }}</span>
-                            <span><img src="/images/dummy.jpg" class="profile"
-                                    style="margin-top: -10px; margin-right:1rem;" alt="">Wiridho
-                                Yu</span>
+                            <span><img src="/storage/profile/{{ $playlist->user->profile }}" class="profile"
+                                    style="margin-top: -10px; margin-right:1rem;"
+                                    alt="">{{ $playlist->user->nama }}</span>
                         </div>
                     </div>
 
                     <div class="row border-top mt-3 pt-3">
-                        <p><b>Video Description</b></p>
-                        <span class="" style="color: #9C9AAA">
-                            {{ $video->deskripsi }}
-                        </span>
+                        <ul class="nav nav-tabs" id="myTab" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link active" id="home-tab" data-bs-toggle="tab"
+                                    data-bs-target="#Description-tab" type="button" role="tab"
+                                    aria-controls="Description-tab" aria-selected="true"><b>Video Description</b></button>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="profile-tab" data-bs-toggle="tab"
+                                    data-bs-target="#Forum-tab" type="button" role="tab" aria-controls="Forum-tab"
+                                    aria-selected="false"><b>Forum</b></button>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="Description-tab" role="tabpanel"
+                                aria-labelledby="home-tab" tabindex="0">{{ $video->deskripsi }}</div>
+                            <div class="tab-pane fade" id="Forum-tab" role="tabpanel" aria-labelledby="profile-tab"
+                                tabindex="0">
+                                <div class="pertanyaan">
+                                    <img src="/storage/profile/{{ auth()->user()->profile }}" class="mentor mt-3"
+                                        style="margin-right:1rem;" alt="">
+                                    <a class="btn border-0 mt-2" data-bs-toggle="collapse" href="#collapseExample"
+                                        role="button" aria-expanded="false" aria-controls="collapseExample">
+                                        <b>Bagaimana cara menginstall boostrap tanpa harus di download</b>
+                                    </a>
+                                    <div class="collapse" id="collapseExample">
+                                        <div class="forum">
+                                            <p> Some placeholder content for the collapse component. This panel is hidden by
+                                                default but revealed when the user activates the relevant trigger.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="pertanyaan">
+                                    <img src="/storage/profile/{{ auth()->user()->profile }}" class="mentor mt-3"
+                                        style="margin-right:1rem;" alt="">
+                                    <a class="btn border-0 mt-2" data-bs-toggle="collapse" href="#collapseExample2"
+                                        role="button" aria-expanded="false" aria-controls="collapseExample">
+                                        <b>Bagaimana cara menginstall boostrap tanpa harus di download</b>
+                                    </a>
+                                    <div class="collapse" id="collapseExample2">
+                                        <div class="forum">
+                                            <p> Some placeholder content for the collapse component. This panel is hidden by
+                                                default but revealed when the user activates the relevant trigger.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
 
