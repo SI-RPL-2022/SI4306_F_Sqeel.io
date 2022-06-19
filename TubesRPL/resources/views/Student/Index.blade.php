1856a66b7c7f2fab007f73c4f6400fd74b9fb9c0
Wiridho Partuaon Tambunan <wiridho@student.telkomuniversity.ac.id>
2022-06-19 22:49:37 +0700

    Dashboard Mentor

--- TubesRPL/resources/views/Student/Index.blade.php
+++ TubesRPL/resources/views/Student/Index.blade.php
@@ -1,15 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@section('content')
+    <div>
+        <h4 class="enrolled">Enrolled Courses (8)</h4>
+        <center>
+            <div class=" d-flex justify-content-evenly flex-wrap mb-5">
+                <div class="  studentContent mb-5">
+                    <img src="/images/card/card.png" width="350px" alt="">
+                    <h5 class="studentTitle">The Complete Web Development <br> Bootcamp</h5>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0"
+                            aria-valuemax="100">
+                            <span class="percent text-center">25%</span>
+                        </div>
 
-<body>
-    <p>Berhasil Login Student</p>
-</body>
+                    </div>
+                </div>
+                <div class="  studentContent">
+                    <img src="/images/card/card.png" width="350px" alt="">
+                    <h5 class="studentTitle">The Complete Web Development <br> Bootcamp</h5>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100">25%</div>
+                    </div>
+                </div>
+                <div class="studentContent">
+                    <img src="/images/card/card.png" width="350px" alt="">
+                    <h5 class="studentTitle">The Complete Web Development <br> Bootcamp</h5>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100">25%</div>
+                    </div>
+                </div>
+                <div class="studentContent">
+                    <img src="/images/card/card.png" width="350px" alt="">
+                    <h5>The Complete Web Development <br> Bootcamp</h5>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100">25%</div>
+                    </div>
+                </div>
+                <div class="studentContent">
+                    <img src="/images/card/card.png" width="350px" alt="">
+                    <h5>The Complete Web Development <br> Bootcamp</h5>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100">25%</div>
+                    </div>
+                </div>
+                <div class="  studentContent">
+                    <img src="/images/card/card.png" width="350px" alt="">
+                    <h5 class="studentTitle">The Complete Web Development <br> Bootcamp</h5>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100">25%</div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <button type="button" class="btn btn-outline loadMore">Load More</button>
+            </div>
 
-</html>
+        </center>
+    </div>
+@endsection
